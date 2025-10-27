26997a7d3366132a426438ea38fbe9a5f45a3837
alexgrot <alexgrot@gmx.de>
2025-10-27 09:51:43 +0100

    better header loading state

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -7,18 +7,26 @@ import { Avatar, AvatarFallback } from "../components/ui/avatar";
 import { Button } from "../components/ui/button";
 
 export default function Header() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const t = useTranslations("Header");
+  const isLoading = status === "loading";
 
   const pathname = usePathname();
 
   return (
-    <header className="flex w-full items-center justify-between border-b border-gray-200 px-6 py-4 shadow">
+    <header
+      className={`flex w-full items-center justify-between border-b border-gray-200 px-6 py-4 shadow relative${isLoading ? "animate-pulse" : ""}`}
+      style={isLoading ? { pointerEvents: "none", opacity: 0.6 } : {}}
+    >
       <div className="flex flex-row items-center gap-2 text-lg font-semibold">
-        <Avatar>
-          <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
-        </Avatar>
-        {session?.user?.name}
+        {session?.user?.name && (
+          <>
+            <Avatar>
+              <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
+            </Avatar>
+            {session?.user?.name}
+          </>
+        )}
       </div>
       <div className="flex flex-row items-center gap-4">
         {session?.user?.isAdmin && (
