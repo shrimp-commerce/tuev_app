9a4241986043ee7b33e2c6b85bb8a9ed18407e98
alexgrot <alexgrot@gmx.de>
2025-12-22 11:10:05 +0100

    hide main page content when not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,13 @@ export default async function Home() {
     <HydrateClient>
       <main className="bg-muted/50 min-h-screen w-full px-2 py-8 md:px-8">
         <div className="mx-auto max-w-5xl space-y-8">
-          <Card>
-            <CardContent className="flex flex-col gap-8">
-              {session?.user && <TodaysTasks />}
-            </CardContent>
-          </Card>
+          {session?.user && (
+            <Card>
+              <CardContent className="flex flex-col gap-8">
+                <TodaysTasks />
+              </CardContent>
+            </Card>
+          )}
 
           {session?.user && <WorkLogs />}
         </div>
